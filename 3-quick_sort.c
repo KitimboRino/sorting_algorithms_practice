3b690d73ca471e1137a9de361be325c998f49781
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2024-01-23 04:38:26 +0000

    feat: quick sort and run tests

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -9,10 +9,12 @@
  */
 void quick_sort(int *array, size_t size)
 {
-	if (array == NULL || size < 2)
-		return;
+    /* Check if the array is NULL or has less than two elements */
+    if (array == NULL || size < 2)
+        return;
 
-	quick_s(array, 0, size - 1, size);
+    /* Call the helper function to perform quick sort on the entire array */
+    quick_s(array, 0, size - 1, size);
 }
 
 /**
@@ -25,49 +27,56 @@ void quick_sort(int *array, size_t size)
  */
 int partition(int *array, int lo, int hi, size_t size)
 {
-	int i = lo - 1, j = lo;
-	int pivot = array[hi], aux = 0;
+    int i = lo - 1, j = lo;
+    int pivot = array[hi], aux = 0;
 
-	for (; j < hi; j++)
-	{
-		if (array[j] < pivot)
-		{
-			i++;
-			if (array[i] != array[j])
-			{
-				aux = array[i];
-				array[i] = array[j];
-				array[j] = aux;
-				print_array(array, size);
-			}
-		}
-	}
-	if (array[i + 1] != array[hi])
-	{
-		aux = array[i + 1];
-		array[i + 1] = array[hi];
-		array[hi] = aux;
-		print_array(array, size);
-	}
-	return (i + 1);
+    /* Iterate through the array to partition it based on the pivot element */
+    for (; j < hi; j++)
+    {
+        if (array[j] < pivot)
+        {
+            i++;
+
+            /* Swap elements if needed and print the array after each swap */
+            if (array[i] != array[j])
+            {
+                aux = array[i];
+                array[i] = array[j];
+                array[j] = aux;
+                print_array(array, size);
+            }
+        }
+    }
+
+    /* Swap the pivot element to its correct position and print the array */
+    if (array[i + 1] != array[hi])
+    {
+        aux = array[i + 1];
+        array[i + 1] = array[hi];
+        array[hi] = aux;
+        print_array(array, size);
+    }
+
+    return (i + 1);
 }
 
 /**
  * quick_s - quick sort
  * @array: given array
  * @lo: lower
- * @hi:higher
+ * @hi: higher
  * @size: array's size
  * Return: void
  */
 void quick_s(int *array, int lo, int hi, size_t size)
 {
-	int pivot;
+    int pivot;
 
-	if (lo < hi)
-	{
-		pivot = partition(array, lo, hi, size);
-		quick_s(array, lo, pivot - 1, size);
-		quick_s(array, pivot + 1, hi, size);
-	}
+    /* Recursively perform quick sort on the subarrays */
+    if (lo < hi)
+    {
+        pivot = partition(array, lo, hi, size);
+        quick_s(array, lo, pivot - 1, size);
+        quick_s(array, pivot + 1, hi, size);
+    }
 }
